da65a9f8c46144dd235a51790ba6ea3223d7036d
mxcoogi <mxcoogi@gmail.com>
2025-02-28 01:39:30 +0900

    절차지향 -> 객체지향 변경

---
+++ src/calculator/v21/App.java
@@ -0,0 +1,30 @@
+package calculator.v21;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=====절차적프로그래밍=====");
+        Scanner sc = new Scanner(System.in);
+        Set<Character> operators = Set.of('+', '-', '*', '/');
+        int num1;
+        int num2;
+        char operator;
+        int res = 0;
+        String temp;
+        //계산시작
+        while (true) {
+
+            //첫 수 입력
+
+            //두 수 입력
+
+            //연산자 입력
+
+            //연산부분
+
+            //결과 출력 및 반복유무
+        }
+    }
+}
