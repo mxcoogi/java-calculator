6de732af96da1c8db2be609e15e41d0b06eafdf4
mxcoogi <mxcoogi@gmail.com>
2025-03-04 19:53:53 +0900

    feat : Add consoleIO

--- src/calculator/v31/ArithmeticCalculator.java
+++ src/calculator/v31/ArithmeticCalculator.java
@@ -3,19 +3,21 @@ package calculator.v31;
 import calculator.v2.Calculator;
 import calculator.v3.operation.OperatorType;
 import calculator.v31.io.ConsoleIO;
-
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class ArithmeticCalculator implements Calculator {
     private Queue<Box> resList;
     private ConsoleIO consoleIO;
     private OperatorType operatorType;
+    private List<Box> numList;
 
 
     ArithmeticCalculator(){
         resList = new LinkedList<>();
         consoleIO = new ConsoleIO();
+        numList = new LinkedList<>();
     }
 
     @Override
@@ -31,5 +33,12 @@ public class ArithmeticCalculator implements Calculator {
     @Override
     public void calculate() {
 
+    }
+
+    public void inputOP() {
+        operatorType = consoleIO.inputOp();
+    }
+    public void inputNum(){
+        numList.add(consoleIO.inputNum());
     }
 }
--- src/calculator/v31/io/ConsoleIO.java
+++ src/calculator/v31/io/ConsoleIO.java
@@ -1,5 +1,57 @@
 package calculator.v31.io;
 
-public class ConsoleIO extends calculator.v3.io.ConsoleIO {
+import calculator.v3.operation.OperatorType;
+import calculator.v31.Box;
+
+import java.util.Scanner;
+
+public class ConsoleIO{
+
+    private Scanner sc;
+
+    public ConsoleIO(){
+        sc = new Scanner(System.in);
+    }
+
+    public void checkExit(String s){
+        if(s.equals("exit")) System.exit(-1);
+    }
+
+    public Box inputNum(){
+        while(true){
+            System.out.print("정수를 입력 해주세요 : ");
+            String temp = sc.nextLine();
+            checkExit(temp);
+            try{
+                if(temp.contains(".")){
+                    return new Box(Double.parseDouble(temp));
+                }else{
+                    return new Box(Long.parseLong(temp));
+                }
+            }catch (NumberFormatException e){
+                System.out.println("올바른 정수를 입력해주세요");
+            }
+        }
+    }
+
+    public OperatorType inputOp(){
+        while(true){
+            System.out.print("+ - * / 중 연산자를 입력해주세요:");
+            String temp = sc.nextLine();
+            checkExit(temp);
+            switch (temp) {
+                case "+":
+                    return OperatorType.ADD;
+                case "-":
+                    return OperatorType.SUB;
+                case "*":
+                    return OperatorType.MUL;
+                case "/":
+                    return OperatorType.DIV;
+                default:
+                    System.out.println("올바른 연산자를 입력해주세요");
+            }
+        }
+    }
 
 }
